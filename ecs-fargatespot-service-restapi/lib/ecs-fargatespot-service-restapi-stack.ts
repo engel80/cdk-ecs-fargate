87c34069c592bd14684385019230aa925328e471
engel80 <prettyhip@gmail.com>
2022-06-24 15:30:36 +0900

    #2 - spot support

--- ecs-fargatespot-service-restapi/lib/ecs-fargatespot-service-restapi-stack.ts
+++ ecs-fargatespot-service-restapi/lib/ecs-fargatespot-service-restapi-stack.ts
@@ -31,7 +31,7 @@ export class FargateSpotRestAPIServiceStack extends Stack {
             vpc,
             securityGroups: [ecsSecurityGroup]
         });
-        const serviceName = 'fargate-restapi'
+        const serviceName = 'fargatespot-restapi'
         const containerName = `${serviceName}-container`
         const applicationPort = 8080;
 
